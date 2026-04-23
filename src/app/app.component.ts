ba188a8bb9e8f7e8f8db23e4b522756cac5ad8eb
Gak1o <gakiotoni@gmail.com>
2026-04-23 15:09:15 +0300

    Refactor styles and update app component structure; add global styles and modify HTML layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { signal, computed } from '@angular/core';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
- username ="";
-
- getUserName(event:Event){
-  this.username = (event.target as HTMLInputElement).value
-  
- }
- setUserName(){
-  this.username="Anto";
- }
- getUserNamewithTemplate(val:string){
-  this.username=val;
-  console.log(val);
- }
+ 
 }
